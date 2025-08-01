ccea77ced70ddfecc14aaaafa6e8652e038f0533
DanielTIDSMC <joaquingarciacobos3@gmail.com>
2025-08-01 15:38:39 -0600

    Fix: Más ajustes en la logica de sensor-monitoring.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serviceWorker: true,
+  serviceWorker: false,  // Deshabilita el service worker Angular para evitar conflictos
   apiUrl: 'https://api.speira.site/api',
   firebaseConfig: {
     apiKey: "AIzaSyCe1-ukYoXmUPeSQqqmmSL_bUnFsY_3hs0",
@@ -10,5 +10,6 @@ export const environment = {
     messagingSenderId: "405388809495",
     appId: "1:405388809495:web:d2d8cde904c3f4e3ec5352",
     measurementId: "G-DWJCN7HR3Y"
-  }
+  },
+  messagingPublicKey: 'BEoRVFWZJfSLaOY4gcvJL6P1x5rSEfbFPspDph1craPCMOnrjjHTRTO0Ux_Ys1OU8g5RBsxL_H0Ko2t1TA5ZFDo'
 };
